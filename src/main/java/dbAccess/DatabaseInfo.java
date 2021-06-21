915f5caad70502a978b2d8da9ff5b80397057e0f
yoshimura-t <yoshimura-t@YOSHIMURA-T-NP.b-eng.it.to-be.co.jp>
2021-06-21 18:05:00 +0900

    aaa

---
+++ src/main/java/dbAccess/DatabaseInfo.java
@@ -0,0 +1,7 @@
+package dbAccess;
+
+public class DatabaseInfo {
+	public static final String DB_URL = "jdbc:mysql://localhost/daiatedb";
+	public static final String USER = "daiateuser";
+	public static final String PASSWORD = "daiatepass";
+}
